59560408143f0bf6272e86e8734485cb6e8f6b11
Antonina Cherednichenko <antonina.cherednichenko@gmail.com>
2016-09-11 17:05:37 +0300

    Add initial implementation of navigation drawer

---
+++ app/src/main/java/com/example/tonya/detox/EmptyFragment.java
@@ -0,0 +1,20 @@
+package com.example.tonya.detox;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by tonya on 9/8/16.
+ */
+public class EmptyFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
--- app/src/main/java/com/example/tonya/detox/ProgramsInfoFragment.java
+++ app/src/main/java/com/example/tonya/detox/ProgramsInfoFragment.java
@@ -1,9 +1,13 @@
 package com.example.tonya.detox;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -17,26 +21,30 @@ import java.util.List;
 
 import jp.wasabeef.recyclerview.animators.SlideInUpAnimator;
 
-public class ReceipesActivity extends AppCompatActivity {
-
+/**
+ * Created by tonya on 9/8/16.
+ */
+public class ProgramsInfoFragment extends Fragment {
     private int[] images = {R.drawable.green_detox_program, R.drawable.citrus_detox_program, R.drawable.apple_detox_program,
             R.drawable.juice_detox_program, R.drawable.rice_detox_program};
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_receipes);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        RecyclerView recList = (RecyclerView) findViewById(R.id.cardList);
+        View rootView = inflater.inflate(R.layout.fragment_receipes, container, false);
+        RecyclerView recList = (RecyclerView) rootView.findViewById(R.id.cardList);
         //recList.setHasFixedSize(true);
         recList.setItemAnimator(new SlideInUpAnimator());
 
-        LinearLayoutManager llm = new LinearLayoutManager(this);
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
         llm.setOrientation(LinearLayoutManager.VERTICAL);
         recList.setLayoutManager(llm);
 
-        ReceipeAdapter adapter = new ReceipeAdapter(this, createReceipeList());
+        ReceipeAdapter adapter = new ReceipeAdapter(getActivity(), createReceipeList());
         recList.setAdapter(adapter);
+
+        return rootView;
     }
 
     private List<ReceipeInfo> createReceipeList() {
@@ -51,11 +59,9 @@ public class ReceipesActivity extends AppCompatActivity {
 
             }
 
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-
 
         return receipes;
     }
