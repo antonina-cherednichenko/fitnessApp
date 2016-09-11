afbd173b63d898a208e45b23b326c55cecefc8ed
Antonina Cherednichenko <antonina.cherednichenko@gmail.com>
2016-09-11 19:43:58 +0300

    Add header for navigation drawer

---
+++ app/src/main/java/com/example/tonya/detox/NavigationDataModel.java
@@ -0,0 +1,17 @@
+package com.example.tonya.detox;
+
+/**
+ * Created by tonya on 9/11/16.
+ */
+public class NavigationDataModel {
+
+    public int icon;
+    public String name;
+
+    // Constructor.
+    public NavigationDataModel(int icon, String name) {
+
+        this.icon = icon;
+        this.name = name;
+    }
+}
