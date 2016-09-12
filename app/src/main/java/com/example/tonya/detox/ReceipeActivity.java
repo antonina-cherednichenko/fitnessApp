61349a240e1dcce619ba45e8dd35257e5a488e28
Antonina Cherednichenko <antonina.cherednichenko@gmail.com>
2016-09-12 22:27:40 +0300

    Add schedule activity

--- app/src/main/java/com/example/tonya/detox/ReceipeActivity.java
+++ app/src/main/java/com/example/tonya/detox/ReceipeActivity.java
@@ -1,12 +1,15 @@
 package com.example.tonya.detox;
 
 import android.content.Intent;
-import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 
 import org.json.JSONArray;
@@ -55,6 +58,17 @@ public class ReceipeActivity extends AppCompatActivity {
         collapsingToolbar.setTitle(this.programName);
         ImageView programImage = (ImageView) findViewById(R.id.toolbar_header_image);
         programImage.setImageResource(images[position]);
+
+        FloatingActionButton schedule = (FloatingActionButton) findViewById(R.id.schedule_floating_button);
+        schedule.setOnClickListener(new FloatingActionButton.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ReceipeActivity.this, ScheduleProgram.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
     private List<DayInfo> createDayList(int position) {
@@ -100,4 +114,8 @@ public class ReceipeActivity extends AppCompatActivity {
         return stringBuilder.toString();
     }
 
+    public boolean onOptionsItemSelected(MenuItem item) {
+        finish();
+        return true;
+    }
 }
