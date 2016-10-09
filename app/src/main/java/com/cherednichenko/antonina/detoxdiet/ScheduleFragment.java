59fb023bae3d78fc57048ac05d61766289b491f5
Antonina Cherednichenko <antonina.cherednichenko@gmail.com>
2016-10-09 14:11:30 +0300

    Add library for calendar view

--- app/src/main/java/com/cherednichenko/antonina/detoxdiet/ScheduleFragment.java
+++ app/src/main/java/com/cherednichenko/antonina/detoxdiet/ScheduleFragment.java
@@ -4,9 +4,21 @@ package com.cherednichenko.antonina.detoxdiet;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.github.tibolte.agendacalendarview.AgendaCalendarView;
+import com.github.tibolte.agendacalendarview.CalendarPickerController;
+import com.github.tibolte.agendacalendarview.models.BaseCalendarEvent;
+import com.github.tibolte.agendacalendarview.models.CalendarEvent;
+import com.github.tibolte.agendacalendarview.models.DayItem;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by tonya on 9/8/16.
@@ -17,7 +29,53 @@ public class ScheduleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_schedule, container, false);
+
+        Calendar minDate = Calendar.getInstance();
+        Calendar maxDate = Calendar.getInstance();
+
+        minDate.add(Calendar.MONTH, -1);
+        minDate.set(Calendar.DAY_OF_MONTH, 1);
+        maxDate.add(Calendar.YEAR, 1);
+
+        List<CalendarEvent> eventList = new ArrayList<>();
+        mockList(eventList);
+
+        ((AgendaCalendarView)rootView.findViewById(R.id.agenda_calendar_view)).init(eventList, minDate, maxDate, Locale.getDefault(), new CalendarPickerController() {
+            @Override
+            public void onDaySelected(DayItem dayItem) {
+
+            }
+
+            @Override
+            public void onEventSelected(CalendarEvent event) {
+
+            }
+
+            @Override
+            public void onScrollToDate(Calendar calendar) {
+
+            }
+        });
+
         return rootView;
+    }
+
+    private void mockList(List<CalendarEvent> eventList) {
+        Calendar startTime1 = Calendar.getInstance();
+        Calendar endTime1 = Calendar.getInstance();
+        endTime1.add(Calendar.MONTH, 1);
+        BaseCalendarEvent event1 = new BaseCalendarEvent("Thibault travels in Iceland", "A wonderful journey!", "Iceland",
+                ContextCompat.getColor(getContext(), R.color.colorPrimary), startTime1, endTime1, true);
+        eventList.add(event1);
+
+        Calendar startTime2 = Calendar.getInstance();
+        startTime2.add(Calendar.DAY_OF_YEAR, 1);
+        Calendar endTime2 = Calendar.getInstance();
+        endTime2.add(Calendar.DAY_OF_YEAR, 3);
+        BaseCalendarEvent event2 = new BaseCalendarEvent("Visit to Dalvík", "A beautiful small town", "Dalvík",
+                ContextCompat.getColor(getContext(), R.color.colorBlue), startTime2, endTime2, true);
+        eventList.add(event2);
+
     }
 
 }
